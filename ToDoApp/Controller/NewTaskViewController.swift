751e924b26ec87639d387fe847549976894886f2
Aleksei Korolev <kab@live.ru>
2021-12-03 20:22:29 +0530

    27

---
+++ ToDoApp/Controller/NewTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewTaskViewController.swift
+//  ToDoApp
+//
+//  Created by Алексей Королев on 03.12.2021.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var dateTextField: UITextField!
+    @IBOutlet var addressTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+    
+
+    @IBAction func saveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+    }
+    
+}
---
+++ ToDoAppTests/Controller/NewTaskViewControllerTests.swift
@@ -0,0 +1,53 @@
+//
+//  NewTaskViewControllerTests.swift
+//  ToDoAppTests
+//
+//  Created by Алексей Королев on 03.12.2021.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class NewTaskViewControllerTests: XCTestCase {
+    
+    var sut: NewTaskViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: NewTaskViewController.self)) as? NewTaskViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testHasTitleTextField() {
+        XCTAssertTrue(sut.titleTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasLocationTextField() {
+        XCTAssertTrue(sut.locationTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasDateTextField() {
+        XCTAssertTrue(sut.dateTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasAddressTextField() {
+        XCTAssertTrue(sut.addressTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasDescriptionTextField() {
+        XCTAssertTrue(sut.descriptionTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasSaveButton() {
+        XCTAssertTrue(sut.saveButton.isDescendant(of: sut.view))
+    }
+    
+    func testHasCancelButton() {
+        XCTAssertTrue(sut.cancelButton.isDescendant(of: sut.view))
+    }
+
+}
